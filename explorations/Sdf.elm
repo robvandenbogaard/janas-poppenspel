5ffd73f546a540d42fde076ccd4f083dee1ca458
Rob van den Bogaard <robvandenbogaard@gmail.com>
2021-02-20 15:56:54 +0100

    Add sdf exploration

---
+++ explorations/Sdf.elm
@@ -0,0 +1,263 @@
+module Sdf exposing (main)
+
+{-
+   Rotating triangle, that is a "hello world" of the WebGL
+-}
+
+import Browser
+import Browser.Dom
+import Browser.Events exposing (onAnimationFrameDelta, onMouseDown, onMouseMove, onMouseUp, onResize)
+import Html exposing (Html)
+import Html.Attributes exposing (height, style, width)
+import Json.Decode as Decode exposing (Decoder, Value)
+import Math.Matrix4 as Mat4 exposing (Mat4)
+import Math.Vector2 as Vec2 exposing (Vec2, vec2)
+import Math.Vector3 as Vec3 exposing (Vec3, vec3)
+import Math.Vector4 as Vec4 exposing (Vec4, vec4)
+import Task
+import WebGL exposing (Mesh, Shader)
+
+
+type Msg
+    = Diff Float
+    | MouseMove Float Float
+    | Press Bool
+    | Resize Float Float
+
+
+type alias Model =
+    { width : Float
+    , height : Float
+    , left : Float
+    , top : Float
+    , pressed : Bool
+    , time : Float
+    }
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init =
+            always
+                ( Model 0 0 0 0 False 0
+                , Task.perform (\{ viewport } -> Resize viewport.width viewport.height) Browser.Dom.getViewport
+                )
+        , view = view
+        , subscriptions = subscriptions
+        , update = update
+        }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Diff time ->
+            ( { model | time = model.time + time }
+            , Cmd.none
+            )
+
+        MouseMove left top ->
+            ( { model | left = left, top = top }
+            , Cmd.none
+            )
+
+        Press pressed ->
+            ( { model | pressed = pressed }
+            , Cmd.none
+            )
+
+        Resize width height ->
+            ( { model | width = width, height = height }
+            , Cmd.none
+            )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions { pressed } =
+    Sub.batch
+        [ onAnimationFrameDelta Diff
+        , onResize (\w h -> Resize (toFloat w) (toFloat h))
+        , onMouseDown (Decode.succeed (Press True))
+        , onMouseUp (Decode.succeed (Press False))
+        , if pressed then
+            onMouseMove mousePosition
+
+          else
+            Sub.none
+        ]
+
+
+mousePosition : Decoder Msg
+mousePosition =
+    Decode.map2 MouseMove
+        (Decode.field "pageX" Decode.float)
+        (Decode.field "pageY" Decode.float)
+
+
+view : Model -> Html msg
+view { width, height, left, top, time } =
+    Html.div []
+        [ WebGL.toHtml
+            [ Html.Attributes.width (round width)
+            , Html.Attributes.height (round height)
+            , Html.Attributes.style "position" "absolute"
+            , Html.Attributes.style "left" "0"
+            , Html.Attributes.style "top" "0"
+            , Html.Attributes.style "display" "block"
+            ]
+            [ WebGL.entity
+                vertexShader
+                fragmentShader
+                mesh
+                { iResolution = vec3 width height 0
+                , iGlobalTime = time / 1000
+                , iMouse = vec4 left top 0 0
+                }
+            ]
+        , Html.text "Shader test"
+        ]
+
+
+
+-- Mesh
+
+
+mesh : Mesh { position : Vec3 }
+mesh =
+    WebGL.triangles
+        [ ( { position = vec3 -1 1 0 }
+          , { position = vec3 1 1 0 }
+          , { position = vec3 -1 -1 0 }
+          )
+        , ( { position = vec3 -1 -1 0 }
+          , { position = vec3 1 1 0 }
+          , { position = vec3 1 -1 0 }
+          )
+        ]
+
+
+
+-- Shaders
+
+
+type alias Uniforms =
+    { iResolution : Vec3
+    , iGlobalTime : Float
+    , iMouse : Vec4
+    }
+
+
+vertexShader : Shader { position : Vec3 } Uniforms { vFragCoord : Vec2 }
+vertexShader =
+    [glsl|
+        precision mediump float;
+        attribute vec3 position;
+        varying vec2 vFragCoord;
+        uniform vec3 iResolution;
+        void main () {
+            gl_Position = vec4(position, 1.0);
+            vFragCoord = (position.xy + 1.0) / 2.0 * iResolution.xy;
+        }
+    |]
+
+
+fragmentShader : Shader {} Uniforms { vFragCoord : Vec2 }
+fragmentShader =
+    [glsl|
+/**
+ * Part 1 Challenges
+ * - Make the circle yellow
+ * - Make the circle smaller by decreasing its radius
+ * - Make the circle smaller by moving the camera back
+ * - Make the size of the circle oscillate using the sin() function and the iTime
+ *   uniform provided by shadertoy
+ */
+precision mediump float;
+varying vec2      vFragCoord;
+uniform vec3      iResolution;           // viewport resolution (in pixels)
+uniform float     iGlobalTime;           // shader playback time (in seconds)
+uniform vec4      iMouse;                // mouse pixel coords. xy: current (if MLB down), zw: click
+
+const int MAX_MARCHING_STEPS = 255;
+const float MIN_DIST = 0.0;
+const float MAX_DIST = 100.0;
+const float EPSILON = 0.0001;
+
+/**
+ * Signed distance function for a sphere centered at the origin with radius 1.0;
+ */
+float sphereSDF(vec3 samplePoint) {
+    return length(samplePoint) - 1.0;
+}
+
+/**
+ * Signed distance function describing the scene.
+ *
+ * Absolute value of the return value indicates the distance to the surface.
+ * Sign indicates whether the point is inside or outside the surface,
+ * negative indicating inside.
+ */
+float sceneSDF(vec3 samplePoint) {
+    return sphereSDF(samplePoint);
+}
+
+/**
+ * Return the shortest distance from the eyepoint to the scene surface along
+ * the marching direction. If no part of the surface is found between start and end,
+ * return end.
+ *
+ * eye: the eye point, acting as the origin of the ray
+ * marchingDirection: the normalized direction to march in
+ * start: the starting distance away from the eye
+ * end: the max distance away from the ey to march before giving up
+ */
+float shortestDistanceToSurface(vec3 eye, vec3 marchingDirection, float start, float end) {
+    float depth = start;
+    for (int i = 0; i < MAX_MARCHING_STEPS; i++) {
+        float dist = sceneSDF(eye + depth * marchingDirection);
+        if (dist < EPSILON) {
+			return depth;
+        }
+        depth += dist;
+        if (depth >= end) {
+            return end;
+        }
+    }
+    return end;
+}
+
+
+/**
+ * Return the normalized direction to march in from the eye point for a single pixel.
+ *
+ * fieldOfView: vertical field of view in degrees
+ * size: resolution of the output image
+ * fragCoord: the x,y coordinate of the pixel in the output image
+ */
+vec3 rayDirection(float fieldOfView, vec2 size, vec2 fragCoord) {
+    vec2 xy = fragCoord - size / 2.0;
+    float z = size.y / tan(radians(fieldOfView) / 2.0);
+    return normalize(vec3(xy, -z));
+}
+
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+	vec3 dir = rayDirection(45.0, iResolution.xy, fragCoord);
+    vec3 eye = vec3(0.0, 0.0, 5.0);
+    float dist = shortestDistanceToSurface(eye, dir, MIN_DIST, MAX_DIST);
+
+    if (dist > MAX_DIST - EPSILON) {
+        // Didn't hit anything
+        fragColor = vec4(0.0, 0.0, 0.0, 0.0);
+		return;
+    }
+
+    fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
+
+void main()
+{ mainImage(gl_FragColor, vFragCoord);
+}
+    |]
